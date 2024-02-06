c254d474138d6c9b1ebc30cea25ff72c2e360034
Yoan <yoan.dubuc@outlook.com>
2024-02-06 16:27:19 -0500

    Initial Commit

---
+++ Perroquet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Perroquet
+//
+//  Created by Yoan Dubuc on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Perroquet/PerroquetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerroquetApp.swift
+//  Perroquet
+//
+//  Created by Yoan Dubuc on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PerroquetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
